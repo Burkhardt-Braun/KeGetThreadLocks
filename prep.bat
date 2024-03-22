d63bdbae90ea26111e2f59ea9376a697d3dc62dd
Burkhardt-Braun <67739115+Burkhardt-Braun@users.noreply.github.com>
2024-03-22 14:37:48 +0100

    Delete prep.bat

--- prep.bat
+++
@@ -1,4 +0,0 @@
-doskey nbuild
-doskey x=clean$Tbuild$Tupdate.bat
-rem doskey k=copy objchk_win7_amd64\amd64\*.pdb e:\ida.hellodriver$Tcopy objchk_win7_amd64\amd64\*.sys e:\ida.hellodriver
-rem doskey k=copy objchk_win7_amd64\amd64\*.pdb e:\ghidra.hellodriver$Tcopy objchk_win7_amd64\amd64\*.sys e:\ghidra.hellodriver
